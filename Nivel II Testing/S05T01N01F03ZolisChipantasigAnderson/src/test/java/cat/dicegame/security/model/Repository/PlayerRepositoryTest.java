113c0d8d0f1f47801cde4a07569c004f20726223
anderson92zolis <azolis92@hotmail.es>
2023-05-14 18:55:27 +0200

    testing all user

--- Nivel II Testing/S05T01N01F03ZolisChipantasigAnderson/src/test/java/cat/dicegame/security/model/Repository/PlayerRepositoryTest.java
+++ Nivel II Testing/S05T01N01F03ZolisChipantasigAnderson/src/test/java/cat/dicegame/security/model/Repository/PlayerRepositoryTest.java
@@ -1,9 +1,16 @@
 package cat.dicegame.security.model.Repository;
 
 import cat.dicegame.security.auth.RegisterRequest;
+import cat.dicegame.security.model.Entity.Player;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import cat.dicegame.security.model.Repository.PlayerRepository;
+
+
+import java.util.Optional;
 
 import static org.assertj.core.api.FactoryBasedNavigableListAssert.assertThat;
 
@@ -13,24 +20,26 @@ class PlayerRepositoryTest {
     @Autowired
     private PlayerRepository playerRepositoryUnderTest;
 
+    @Autowired
+    private final ModelMapper modelMapper = new ModelMapper();
+
     @Test
-    public void randomNumbersTest() {
+    public void findByEmailTest() {
 
         //give
 
         RegisterRequest registerRequest = new RegisterRequest(
-            "name", "email", "password"
+            "name", "b@gmail.com", "password"
             );
 
-        playerRepositoryUnderTest.save(registerRequest);
+        playerRepositoryUnderTest.save(modelMapper.map(registerRequest, Player.class));
 
         //when
 
-        boolean exists = playerRepositoryUnderTest.findByEmail("email");
+        Optional<Player> foundPlayer = playerRepositoryUnderTest.findByEmail("b@gmail.com");
 
         //then
-
-        assertThat(exists).isTrue();
+        Assertions.assertThat(foundPlayer.isPresent());
 
     }
 
--- Nivel II Testing/S05T01N01F03ZolisChipantasigAnderson/src/test/java/cat/dicegame/security/model/Service/DiceGameServiceImplemTest.java
+++ Nivel II Testing/S05T01N01F03ZolisChipantasigAnderson/src/test/java/cat/dicegame/security/model/Service/DiceGameServiceImplemTest.java
@@ -1,102 +1,167 @@
 package cat.dicegame.security.model.Service;
 
+import cat.dicegame.security.model.Dto.PlayerDto;
+import cat.dicegame.security.model.Entity.Player;
 import cat.dicegame.security.model.Repository.PlayerRepository;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import java.util.Collections;
+import java.util.List;
+import java.util.NoSuchElementException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.client.ExpectedCount.times;
 
 //  https://www.youtube.com/watch?v=Geq60OVyBPg
+// https://www.baeldung.com/mockito-junit-5-extension
+@ExtendWith(MockitoExtension.class)
 class DiceGameServiceImplemTest {
 
     @Mock
     private PlayerRepository playerRepository;
+    private ModelMapper mapper;
     private AutoCloseable autoCloseable;
     private DiceGameServiceImplem underTest;
+
+
 
     @BeforeEach
     void setUp() {
         autoCloseable=MockitoAnnotations.openMocks(this);
         underTest = new DiceGameServiceImplem(playerRepository);
-    }
 
+    }
     @AfterEach
     void tearDown() throws Exception {
         autoCloseable.close();
     }
 
     @Test
+    @Disabled
     void convertPlayerEntitytoDTO() {
     }
 
     @Test
+    @Disabled
     void convertPlayerDTOtoEntity() {
     }
 
     @Test
+    @Disabled
     void createPlayer() {
     }
 
     @Test
+    @Disabled
     void updatePlayer() {
     }
 
     @Test
+    @Disabled
     void createRoll() {
     }
 
     @Test
+    @Disabled
     void deleteRollsofAPlayer() {
     }
 
     @Test
+    @Disabled
     void deleteUser() {
     }
 
     @Test
-    void getAllUsersInTheGame() {
+    void getAllUsersInTheGameTest() throws NoSuchElementException {
+        /*
         // when
-        underTest.getAllUsersInTheGame();
-        // then
+
+        Player player = new Player();
+        player.setName("John");
+        player.setEmail("john@example.com");
+
+        when(playerRepository.findAll()).thenReturn(Collections.singletonList(player));
+
+        // Execute the method
+
+        List<PlayerDto> result = underTest.getAllUsersInTheGame();
+
+        // Verify the interactions and assertions
         verify(playerRepository).findAll();
+
+        assertEquals(1, result.size());
+        PlayerDto playerDto = result.get(0);
+        assertEquals("John", playerDto.getName());
+        assertEquals("john@example.com", playerDto.getEmail());
+
+*/
+
+        underTest.getAllUsersInTheGame() ;
+
+        // then
+
+
+        verify(playerRepository).findAll();
+
+
     }
 
     @Test
+    @Disabled
     void getPlayerDtoByIdWithOverage() {
     }
 
     @Test
+    @Disabled
     void averageSuccessRateGetONE() {
     }
 
     @Test
+    @Disabled
     void getOveragesRankingOfAllPlayer() {
     }
 
     @Test
+    @Disabled
     void getPlayerWithTheWorstLossRate() {
     }
 
     @Test
+    @Disabled
     void getPlayerWithTheWorstSuccessRate() {
     }
 
     @Test
+    @Disabled
     void convertListOfPlayerToListOfPLayerDTO() {
     }
 
     @Test
+    @Disabled
     void exitsById() {
     }
 
     @Test
+    @Disabled
     void verifyPlayerName() {
     }
 
     @Test
+    @Disabled
     void getPlayerById() {
     }
 }
